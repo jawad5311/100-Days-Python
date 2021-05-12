aab554e8ac4301add25c9a3db565602b9d3de6bb
Jawad <muhammadjawad5311@gmail.com>
2021-05-12 15:43:20 +0500

    turtle collision with cars functionality added

--- intermediate-Day15-to-Day40/Day_23-turtle_crossing_game/main.py
+++ intermediate-Day15-to-Day40/Day_23-turtle_crossing_game/main.py
@@ -11,23 +11,28 @@ screen.bgcolor("gray")
 screen.tracer(0)
 
 player = Player()
-car = CarManager()
+car_manager = CarManager()
 score = Scoreboard()
 
 screen.listen()
 screen.onkeypress(player.move_forward, "Up")
 
 
-game_is_on = True
-while game_is_on:
+game_on = True
+while game_on:
     time.sleep(0.1)
     screen.update()
 
-    car.create_car()
-    car.move_cars()
+    car_manager.create_car()
+    car_manager.move_cars()
 
     if player.ycor() > 280:
         player.reset_turtle()
-        car.increase_speed()
+        car_manager.increase_speed()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_on = False
+
 
 screen.exitonclick()
