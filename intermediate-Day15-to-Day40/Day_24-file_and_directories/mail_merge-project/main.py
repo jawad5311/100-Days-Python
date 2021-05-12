f1b8c37d3b6f2fe7462dee2ab04a26e4218a7fb1
Jawad <muhammadjawad5311@gmail.com>
2021-05-13 00:54:17 +0500

    Day 25 READEME.md

--- intermediate-Day15-to-Day40/Day_24-file_and_directories/mail_merge-project/main.py
+++ intermediate-Day15-to-Day40/Day_24-file_and_directories/mail_merge-project/main.py
@@ -18,11 +18,5 @@ with open("starting_letter.txt") as letter_file:
         f_name = name.strip()  # Strip empty spaces from the string
         # Replace [name] with user name
         new_letter = starting_letter.replace("[name]", f_name)
-        print(new_letter)
         with open(f"output/letter_for_{f_name}.txt", "w") as user_file:
-            user_file.write(new_letter)  # Write new formatted letter 
-
-
-
-
-
+            user_file.write(new_letter)  # Write new formatted letter
---
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/README.md
@@ -0,0 +1,11 @@
+# Working with CSV files and Pandas
+
+#### Day 25
+
+##### ACTIVITIES
+
+Working with CSV Files
+
+Analysing Data with Pandas
+
+States guessing game Project
