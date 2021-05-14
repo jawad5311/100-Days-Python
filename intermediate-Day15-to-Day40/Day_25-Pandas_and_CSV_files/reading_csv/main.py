0649c14e91fbf2d839bbd1acae25c89ba645dc38
Jawad <muhammadjawad5311@gmail.com>
2021-05-14 21:03:00 +0500

    Reading CSV files using Pandas

--- intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/reading_csv/main.py
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/reading_csv/main.py
@@ -1,5 +1,6 @@
 
 import csv
+import pandas
 
 # with open("weather_data.csv") as file:
 #     weather_data = file.readlines()
@@ -8,16 +9,14 @@ import csv
 
 temperatures = []
 
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-
-    print(temperatures)
-
-
-
-
-
+# Reading data using pandas
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])  # Reading data from "temp" column
