6d447f1d2801e226b9d33222598f4d44ce6f845d
Jawad <muhammadjawad5311@gmail.com>
2021-05-14 22:14:23 +0500

    practice on data using pandas

--- intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/reading_csv/main.py
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/reading_csv/main.py
@@ -1,22 +1,34 @@
 
-import csv
+
 import pandas
 
-# with open("weather_data.csv") as file:
-#     weather_data = file.readlines()
-#
-# print(weather_data)
-
-temperatures = []
-
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#
-#     print(temperatures)
 
 # Reading data using pandas
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])  # Reading data from "temp" column
+# print(data["temp"])  # Reading data from "temp" column
+
+# temp_list = data["temp"].to_list()
+# # avg_temp = sum(temp_list)/len(temp_list)
+# # print(avg_temp)
+#
+# print(temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# Accessing data of the row with high temp
+print(data[data.temp == data.temp.max()])
+
+
+# # Accessing data from row "Monday" and converting its temp value from C to F
+# monday = data[data.day == "Monday"]
+# monday_temp_f = (monday.temp * 9/5) + 32
+# print(monday_temp_f)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
