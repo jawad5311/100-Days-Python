b889f5a161a9657554732f298c458545b107f0ef
Jawad <muhammadjawad5311@gmail.com>
2021-05-15 15:26:21 +0500

    Completed game working and formatted code

--- intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
@@ -2,33 +2,42 @@
 import turtle
 import pandas as pd
 
+# Create screen object and set states_img as background
 screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
-screen.addshape(image)
+screen.addshape(image)  # Add user img to the turtle shape list
 turtle.shape(image)
-tim = turtle.Turtle()
 
+# Create turtle object
+tim = turtle.Turtle()
+tim.hideturtle()
+tim.penup()
+
+# Stores all the correct guessed states
 correct_guesses = []
 
 states_data = pd.read_csv("50_states.csv")
 
+# Checks if user have guessed all the states
 check = len(correct_guesses) < len(states_data.state)
 score = 0
 total_score = len(states_data.state)
 
+# Keeps loop running while the user have not guessed all the states
 while check:
+    # Takes user input and display scores on screen
     text = f"{score}/{total_score} States Correct"
     answer_state = screen.textinput(text, "What's another state's name?").title().strip()
-    print(type(answer_state))
     guessed_state = states_data[states_data["state"] == answer_state]
-    if check:
-        tim.goto(int(guessed_state.x), int(guessed_state.y))
-        tim.write(f"{answer_state}")
-        correct_guesses.append(answer_state)
-        print(correct_guesses)
-        score += 1
 
+    # Check if guessed state is in states list
+    if answer_state in states_data.state.to_list():
+        # Make turtle go to the x and y co-ordinates of guessed states
+        tim.goto(int(guessed_state.x), int(guessed_state.y))
+        tim.write(f"{answer_state}")  # Write the guessed state name
+        correct_guesses.append(answer_state)  # append the correct guess in list
+        score += 1
 
 
 screen.exitonclick()
