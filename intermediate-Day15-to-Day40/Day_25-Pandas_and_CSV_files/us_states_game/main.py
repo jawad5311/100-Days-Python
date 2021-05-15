49d85858b6a2bcd571db8c97606c3b2838b48ce6
Jawad <muhammadjawad5311@gmail.com>
2021-05-15 14:58:11 +0500

    US States Game, Initial raw code without format

--- intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
@@ -1,15 +1,34 @@
 
 import turtle
-
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+tim = turtle.Turtle()
 
+correct_guesses = []
 
-answer_state = screen.textinput("Guess the State", "What's another state's name?")
+states_data = pd.read_csv("50_states.csv")
+
+check = len(correct_guesses) < len(states_data.state)
+score = 0
+total_score = len(states_data.state)
+
+while check:
+    text = f"{score}/{total_score} States Correct"
+    answer_state = screen.textinput(text, "What's another state's name?").title().strip()
+    print(type(answer_state))
+    guessed_state = states_data[states_data["state"] == answer_state]
+    if check:
+        tim.goto(int(guessed_state.x), int(guessed_state.y))
+        tim.write(f"{answer_state}")
+        correct_guesses.append(answer_state)
+        print(correct_guesses)
+        score += 1
+
 
 
 screen.exitonclick()
