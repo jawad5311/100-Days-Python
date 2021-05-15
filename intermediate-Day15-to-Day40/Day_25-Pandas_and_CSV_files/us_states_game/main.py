23ae53cb0b7e0c2c735d026d4246d10ff6980aa1
Jawad <muhammadjawad5311@gmail.com>
2021-05-15 15:49:02 +0500

    convert missing states to csv, functionality added.

--- intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
+++ intermediate-Day15-to-Day40/Day_25-Pandas_and_CSV_files/us_states_game/main.py
@@ -14,22 +14,28 @@ tim = turtle.Turtle()
 tim.hideturtle()
 tim.penup()
 
+states_data = pd.read_csv("50_states.csv")
+
 # Stores all the correct guessed states
 correct_guesses = []
-
-states_data = pd.read_csv("50_states.csv")
+states_list = states_data.state.to_list()
 
 # Checks if user have guessed all the states
 check = len(correct_guesses) < len(states_data.state)
-score = 0
 total_score = len(states_data.state)
 
 # Keeps loop running while the user have not guessed all the states
 while check:
     # Takes user input and display scores on screen
-    text = f"{score}/{total_score} States Correct"
+    text = f"{len(correct_guesses)}/{total_score} States Correct"
     answer_state = screen.textinput(text, "What's another state's name?").title().strip()
     guessed_state = states_data[states_data["state"] == answer_state]
+
+    # Ends while loop if user enter exit and create the csv of missing states
+    if answer_state == "Exit":
+        missing_states = pd.DataFrame(states_list)
+        missing_states.to_csv("states_to_learn.csv")
+        break
 
     # Check if guessed state is in states list
     if answer_state in states_data.state.to_list():
@@ -37,7 +43,4 @@ while check:
         tim.goto(int(guessed_state.x), int(guessed_state.y))
         tim.write(f"{answer_state}")  # Write the guessed state name
         correct_guesses.append(answer_state)  # append the correct guess in list
-        score += 1
-
-
-screen.exitonclick()
+        states_list.remove(answer_state)
