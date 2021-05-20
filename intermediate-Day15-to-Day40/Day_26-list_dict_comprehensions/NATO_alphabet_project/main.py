5e220081cc5ac047914e0399a3fbb33de507090f
Jawad <muhammadjawad5311@gmail.com>
2021-05-20 14:47:24 +0500

    Nato Phonetic project updated | KeyError exception added

--- intermediate-Day15-to-Day40/Day_26-list_dict_comprehensions/NATO_alphabet_project/main.py
+++ intermediate-Day15-to-Day40/Day_26-list_dict_comprehensions/NATO_alphabet_project/main.py
@@ -14,10 +14,18 @@ nato_alphabets = pd.read_csv("nato_phonetic_alphabet.csv")
 # Converts pandas Dataframe into python dict
 nato_dict = {row.letter:row.code for (index, row) in nato_alphabets.iterrows()}
 
-# Removes all the white spaces
-user_inp = input("Your name?").replace(" ", "")
 
-# Return the list based on user input
-phonetic_code = [nato_dict[letter] for letter in user_inp.upper()]
+def generate_phonetic():
+    # Removes all the white spaces
+    user_inp = input("Your name?").replace(" ", "")
+    try:
+        # Return the list based on user input
+        phonetic_code = [nato_dict[letter] for letter in user_inp.upper()]
+    except KeyError:
+        print("Please write name in alphabets. Not in numbers")
+        generate_phonetic()
+    else:
+        print(phonetic_code)
 
-print(phonetic_code)
+
+generate_phonetic()
