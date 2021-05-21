0f98ff60f9ce558f1e22126504d637c298e539d6
Jawad <muhammadjawad5311@gmail.com>
2021-05-21 15:52:51 +0500

    Reading data from .csv | Project completed | Code Formatted

--- intermediate-Day15-to-Day40/Day_31-flash_card_app/main.py
+++ intermediate-Day15-to-Day40/Day_31-flash_card_app/main.py
@@ -1,3 +1,4 @@
+
 import random
 import tkinter
 import pandas
@@ -7,26 +8,48 @@ import pandas
 BACKGROUND_COLOR = "#B1DDC6"
 
 
+# Global Variables
+rand_word = None
+
+
 """
     Displaying random word on screen
 
 """
 
-data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
-# print(data)
-# print(rand_word)
-# print(type(rand_word))
-# print(rand_word["French"])
-# print(rand_word["English"])
-
-# data.remove(rand_word)
+try:  # Try to open the words_to_learn.csv file
+    data = pandas.read_csv("data/words_to_learn.csv").to_dict(orient="records")
+except FileNotFoundError:  # And If the file was not found then open the initial data file
+    data = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
 
 
 def generate_word():
-    rand_word = random.choice(data)
-    fr_word = rand_word["French"]
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=fr_word)
+    """ Generates a random word from the available french words list"""
+    global rand_word
+
+    def display_eng_word():
+        """ Display the english word of the current french word """
+        en_word = rand_word["English"]  # Grabs the English word of the current word
+        canvas.itemconfig(card_title, text="English", fill="white")  # Change screen title to English
+        canvas.itemconfig(card_word, text=en_word, fill="white")  # Display the english word of the current displaying french word
+        canvas.itemconfig(canvas_image, image=back_image)  # Changes the background
+
+    rand_word = random.choice(data)  # Choose a random word from the data
+
+    fr_word = rand_word["French"]  # Grabs the French word of the current word
+    canvas.itemconfig(canvas_image, image=front_image)  # Changes the background
+    canvas.itemconfig(card_title, text="French", fill="black")  # Change screen title to French
+    canvas.itemconfig(card_word, text=fr_word, fill="black")  # Display the french word
+    canvas.after(3000, display_eng_word)  # Wait for 3sec and call the function to display english word
+
+
+def known_word():
+    """ Updates the .csv file if the word is known to user and generate a new word """
+    global rand_word
+    data.remove(rand_word)  # Removes the current random word from the data
+    to_learn = pandas.DataFrame(data)  # Creates a data frame of the updated data
+    to_learn.to_csv("data/words_to_learn.csv", index=False)  # Creates & Updates the .csv file with updated data
+    generate_word()
 
 
 """
@@ -46,19 +69,19 @@ back_image = tkinter.PhotoImage(file="images/card_back.png")
 
 # Canvas Object that holds background image and Title & Label
 canvas = tkinter.Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=front_image)
-canvas.create_image(400, 263, image=back_image)
+canvas_image = canvas.create_image(400, 263, image=front_image)
+#canvas.create_image(400, 263, image=back_image)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Right and Wrong Buttons
-right_btn = tkinter.Button(image=right_image, command=generate_word, highlightthickness=0, bg=BACKGROUND_COLOR)
-right_btn.grid(column=0, row=1)
+right_btn = tkinter.Button(image=right_image, command=known_word, highlightthickness=0, bg=BACKGROUND_COLOR)
+right_btn.grid(column=1, row=1)
 wrong_btn = tkinter.Button(image=wrong_image, command=generate_word, bg=BACKGROUND_COLOR, highlightthickness=0)
-wrong_btn.grid(column=1, row=1)
+wrong_btn.grid(column=0, row=1)
 
-generate_word()
+generate_word()  # Generates the initial word on the screen
 
 window.mainloop()  # Keep window running
