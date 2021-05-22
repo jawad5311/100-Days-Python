557590dbdf052bb3cc3a3547dd65c57b8e830dc7
Jawad <muhammadjawad5311@gmail.com>
2021-05-22 15:26:56 +0500

    ISS tracker completed & formatted

--- intermediate+_Day_33_to_40/Day_33-API_endpoints_parameters/api_parameters/main.py
+++ intermediate+_Day_33_to_40/Day_33-API_endpoints_parameters/api_parameters/main.py
@@ -1,5 +1,5 @@
+import smtplib
 import time
-
 import requests
 import datetime as dt
 
@@ -8,36 +8,45 @@ import datetime as dt
 MY_LAT = 33.684422
 MY_LONG = 73.047882
 
+# Parameter to request sunrise and sunset data
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "formatted": 0
 }
 
-while True:
-
+while True:  # This loop will run for every 60 sec
+    # Send request and holds ISS lat & long
     iss_response = requests.get("http://api.open-notify.org/iss-now.json")
     iss_data = iss_response.json()
+    iss_lat = float(iss_data["iss_position"]["latitude"])  # ISS latitude
+    iss_lng = float(iss_data["iss_position"]["longitude"])  # ISS Longitude
 
-    iss_lat = float(iss_data["iss_position"]["latitude"])
-    iss_lng = float(iss_data["iss_position"]["longitude"])
-
-    response = requests.get("https://api.sunrise-sunset.org/json", parameters)
-    response.raise_for_status()
-    data = response.json()
-
-    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 4
-    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 6
-
-    print(sunrise)
-    print(sunset)
-
-    time_now = dt.datetime.now()
-    print(time_now.hour)
+    time_now = dt.datetime.now()  # My current time
 
     if MY_LAT - 5 < iss_lat < MY_LAT + 5 and MY_LONG - 5 < iss_lng < MY_LONG + 5:
-        if sunrise > time_now.hour > sunset:
-            print("check")
+        # If ISS is in my location then it will chech if its day or night in my location
+        response = requests.get("https://api.sunrise-sunset.org/json", parameters)
+        response.raise_for_status()
+        data = response.json()
+        # Sunrise and sunset hours
+        sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 4
+        sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 6
 
-    time.sleep(60.0)
+        if sunrise > time_now.hour > sunset:  # Sends email if it's night
+            # with smtplib.SMTP("smtp.gmail.com") as connection:
+            #     connection.starttls()
+            #     connection.login(
+            #         user="myname@email.com",
+            #         password="password34-9("
+            #     )
+            #     connection.sendmail(
+            #         from_addr="myname@email.com",
+            #         to_addrs="reciever@email.com",
+            #         msg="Subject: Watch the SKY\n\n"
+            #             "ISS is in moving above Islamabad right now. "
+            #             "Go out and try to find it in the sky..."
+            #     )
+            pass
 
+    time.sleep(60.0)  # Wait for 60 secs
