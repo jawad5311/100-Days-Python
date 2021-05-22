93723a382f8839d2e2c813027a3adee9cdb5e7ed
Jawad <muhammadjawad5311@gmail.com>
2021-05-22 14:43:07 +0500

    Latitude & Longitute and Sunrise & Sunset check added

--- intermediate+_Day_33_to_40/Day_33-API_endpoints_parameters/api_parameters/main.py
+++ intermediate+_Day_33_to_40/Day_33-API_endpoints_parameters/api_parameters/main.py
@@ -1,3 +1,4 @@
+import time
 
 import requests
 import datetime as dt
@@ -7,28 +8,36 @@ import datetime as dt
 MY_LAT = 33.684422
 MY_LONG = 73.047882
 
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "formatted": 0
 }
 
+while True:
 
-response = requests.get("https://api.sunrise-sunset.org/json", parameters)
-response.raise_for_status()
-data = response.json()
+    iss_response = requests.get("http://api.open-notify.org/iss-now.json")
+    iss_data = iss_response.json()
 
-print(data)
+    iss_lat = float(iss_data["iss_position"]["latitude"])
+    iss_lng = float(iss_data["iss_position"]["longitude"])
 
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 5
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 5
+    response = requests.get("https://api.sunrise-sunset.org/json", parameters)
+    response.raise_for_status()
+    data = response.json()
 
-print(sunrise, "\n",sunset)
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 4
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 6
 
+    print(sunrise)
+    print(sunset)
 
-time_now = dt.datetime.now()
-print(time_now.hour)
+    time_now = dt.datetime.now()
+    print(time_now.hour)
 
+    if MY_LAT - 5 < iss_lat < MY_LAT + 5 and MY_LONG - 5 < iss_lng < MY_LONG + 5:
+        if sunrise > time_now.hour > sunset:
+            print("check")
 
+    time.sleep(60.0)
 
