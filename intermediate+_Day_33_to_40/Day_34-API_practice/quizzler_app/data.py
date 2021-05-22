47ac83f45fa40b6245bb6b7ae0111d7bdbe43631
Jawad <muhammadjawad5311@gmail.com>
2021-05-22 16:08:34 +0500

    data.py modified | api request added

--- intermediate+_Day_33_to_40/Day_34-API_practice/quizzler_app/data.py
+++ intermediate+_Day_33_to_40/Day_34-API_practice/quizzler_app/data.py
@@ -1,6 +1,17 @@
 
 import requests
 
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+data = requests.get("https://opentdb.com/api.php", parameters).json()
+
+question_data = data["results"]
+print(question_data)
+
+
 
 
 # question_data = [
