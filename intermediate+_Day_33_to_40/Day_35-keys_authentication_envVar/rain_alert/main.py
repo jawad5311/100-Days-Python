7286d66a726c9129bfdf3e6497fb55856f6e20d4
Jawad <muhammadjawad5311@gmail.com>
2021-05-23 13:26:18 +0500

    Rain Alert | main.py updated | env var created

--- intermediate+_Day_33_to_40/Day_35-keys_authentication_envVar/rain_alert/main.py
+++ intermediate+_Day_33_to_40/Day_35-keys_authentication_envVar/rain_alert/main.py
@@ -1,17 +1,13 @@
 
 import requests
+import os
+from decouple import config
 
-MY_LAT = 47.751076
-MY_LONG = -120.740135
-API_KEY = "94fb4ed33b056db390dc54823c14f955"
+MY_LAT = config("LAT")
+MY_LONG = config("LNG")
+# API_KEY = os.environ.get("OWM_API_KEY")
+API_KEY = config("API_KEY")
 exclude = "current,minutely,daily,alerts"
-
-
-# parameters = {
-#     "lat": MY_LAT,
-#     "lon": MY_LONG,
-#     "aapid": 94fb4ed33b056db390dc54823c14f955
-# }
 
 
 response = requests.get(
@@ -22,15 +18,15 @@ print(response.status_code)
 response.raise_for_status()
 weather_data = response.json()
 
-hourly_data = weather_data["hourly"][:12]
+data_12_hours = weather_data["hourly"][:12]
 
 index = 0
-id_code = hourly_data[index]["weather"][0]["id"]
 
 will_rain = False
 
-for _ in hourly_data:
-    rain_code = int(hourly_data[index]["weather"][0]["id"])
+for _ in data_12_hours:
+    rain_code = int(data_12_hours[index]["weather"][0]["id"])
+    index += 1
     if rain_code < 700:
         will_rain = True
 
