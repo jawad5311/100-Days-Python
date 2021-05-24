03cb18eb3d8ea5fd5c2b1c256c5d71ff0ca2a6b9
Jawad <muhammadjawad5311@gmail.com>
2021-05-24 12:29:32 +0500

    pixel update & delete | project completed | code formatted

--- intermediate+_Day_33_to_40/Day_37-habit_tracking_project/habit_tracking_app/main.py
+++ intermediate+_Day_33_to_40/Day_37-habit_tracking_project/habit_tracking_app/main.py
@@ -1,11 +1,12 @@
 
 import requests
 import decouple
+import datetime
 
 """
-    Following parameters and endpoints are used to create account on pixe.la 
+    Creating an account on pixe.la 
 """
-pixela_user = decouple.config('USERNAME')
+pixela_user = "jawad5311"
 user_token = decouple.config('PIXELA_API')
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -15,12 +16,14 @@ user_parameters = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-
 # response = requests.post(url=pixela_endpoint, json=user_parameters)
 # print(response.text)
 # print(response.status_code)
 
 
+"""
+    Creating a graph on pixela
+"""
 graph_endpoint = f"https://pixe.la/v1/users/jawad5311/graphs"
 graph_id = "graph1"
 
@@ -32,33 +35,60 @@ graph_parameters = {
     "color": "sora",
 }
 
+""" Header Token use for authentication."""
 headers = {
     "X-USER-TOKEN": user_token
 }
-
 # graph_response = requests.post(
 #     url=graph_endpoint,
 #     headers=headers,
 #     json=graph_parameters
 # )
-
 # print(graph_response.status_code)
 # print(graph_response.text)
 
+
+"""
+    Creating pixel on graph
+"""
 post_pix_endpoint = f"https://pixe.la/v1/users/jawad5311/graphs/{graph_id}/"
+today = datetime.datetime.now().strftime("%Y%m%d")
 
 pixel_parameters = {
-    "date":"20210522",
-    "quantity":"7",
+    "date": today,
+    "quantity":"17",
     "optionalData":"{\"work\":\"Trading Alert App\"}"
 }
+# post_pixel = requests.post(
+#     url=post_pix_endpoint,
+#     headers= headers,
+#     json= pixel_parameters
+# )
+# print(post_pixel.status_code)
+# print(post_pixel.text)
 
-post_pixel = requests.post(
-    url=post_pix_endpoint,
-    headers= headers,
-    json= pixel_parameters
-)
 
-print(post_pixel.status_code)
-print(post_pixel.text)
+"""
+    Updating pixel value using put request
+"""
+update_pix_endpoint = f"https://pixe.la/v1/users/{pixela_user}/graphs/{graph_id}/{today}"
+update_pix_parameters = {
+    "quantity": "9"
+}
+# update_pixel = requests.put(
+#     url= update_pix_endpoint,
+#     headers= headers,
+#     json= update_pix_parameters
+# )
+# print(update_pixel.status_code)
+# print(update_pixel.text)
 
+
+""" Delete the pixel using delete request """
+# delete_pixel = requests.delete(
+#     url= f"https://pixe.la/v1/users/{pixela_user}/graphs/{graph_id}/20210522",
+#     headers= headers
+# )
+#
+# print(delete_pixel.status_code)
+# print(delete_pixel.text)
