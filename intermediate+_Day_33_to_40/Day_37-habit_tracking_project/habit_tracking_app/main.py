318c07bbd369d3c7215697d26862c2a935768e58
Jawad <muhammadjawad5311@gmail.com>
2021-05-24 11:30:16 +0500

    graph created using post request

--- intermediate+_Day_33_to_40/Day_37-habit_tracking_project/habit_tracking_app/main.py
+++ intermediate+_Day_33_to_40/Day_37-habit_tracking_project/habit_tracking_app/main.py
@@ -5,20 +5,44 @@ import decouple
 """
     Following parameters and endpoints are used to create account on pixe.la 
 """
-pixela_endpoints = "https://pixe.la/v1/users"
+pixela_user = decouple.config('USERNAME')
+user_token = decouple.config('PIXELA_API')
+
+pixela_endpoint = "https://pixe.la/v1/users"
 user_parameters = {
-    "token": decouple.config('PIXELA_API'),
-    "username": decouple.config('USERNAME'),
+    "token": user_token,
+    "username": pixela_user,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-# response = requests.post(url=pixela_endpoints, json=user_parameters)
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
 # print(response.text)
 # print(response.status_code)
 
 
+graph_endpoint = f"https://pixe.la/v1/users/jawad5311/graphs"
 
+graph_parameters = {
+    "id": "graph1",
+    "name": "Coding_Habit",
+    "unit": "min",
+    "type": "int",
+    "color": "sora",
+}
+
+headers = {
+    "X-USER-TOKEN": user_token
+}
+
+# graph_response = requests.post(
+#     url=graph_endpoint,
+#     headers=headers,
+#     json=graph_parameters
+# )
+
+# print(graph_response.status_code)
+# print(graph_response.text)
 
 
 
