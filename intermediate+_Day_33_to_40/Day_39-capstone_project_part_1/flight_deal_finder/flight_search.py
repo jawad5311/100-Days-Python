5f9efa5f17bd1b8be87d02c52a9bd798b999953c
Jawad <muhammadjawad5311@gmail.com>
2021-05-25 16:06:53 +0500

    Search for flight and return flight data

--- intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/flight_search.py
+++ intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/flight_search.py
@@ -1,21 +1,32 @@
 
 import os
 from pprint import pprint
-
-import dotenv
+import datetime as dt
 import requests
 
+from flight_data import FlightData
 
+import dotenv
 dotenv.load_dotenv()
 
 kiwi_api = os.environ.get('KIWI_API')
-kiwi_endpoint = "https://tequila-api.kiwi.com/locations/query"
+kiwi_endpoint = "https://tequila-api.kiwi.com/v2/search"
 kiwi_header = {
     'apikey': kiwi_api
 }
 
 
-class FlightSearch:
+current_time = dt.datetime.now()
+from_date = (current_time + dt.timedelta(days=3)).strftime('%d/%m/%Y')
+to_date = (current_time + dt.timedelta(days=180)).strftime('%d/%m/%Y')
+
+
+class FlightSearch():
+    # def __init__(self):
+    #     # self.price = int(price)
+    #     # self.air_code = departure_airport_code
+    #     # self.depar_city = departure_city
+    #     pass
 
     def get_destination_code(city_name):
         kiwi_parameters = {
@@ -31,10 +42,49 @@ class FlightSearch:
         #print(response.text)
 
         code_data = response.json()['locations'][0]['code']
-        pprint(code_data)
+        # pprint(code_data)
         return code_data
 
+    def search_fare_price(self, origin_code, destination_city_code, from_date, to_date):
+        kiwi_parameters = {
+            "fly_from": origin_code,
+            "fly_to": destination_city_code,
+            "date_from": from_date,
+            "date_to": to_date,
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "USD"
+        }
+        response = requests.get(
+            url=kiwi_endpoint,
+            headers= kiwi_header,
+            params= kiwi_parameters
+        )
+        # print(response.status_code)
+        # print(response.json())
 
-# fs = FlightSearch()
-#
-# fs.get_destination_code('paris')
+        try:
+            data = response.json()['data'][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        # print(data)
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: ${flight_data.price}")
+        return flight_data
+
+
+
--- intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/main.py
+++ intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/main.py
@@ -2,6 +2,7 @@
 import os
 import requests
 from pprint import pprint
+from datetime import datetime, timedelta
 
 import flight_search
 import data_manager
@@ -21,7 +22,21 @@ flight_search = flight_search.FlightSearch()
 sheet_data = data_manager.get_destination_data()
 pprint(sheet_data)
 
-data_manager.update_destination_code()
-pprint(sheet_data)
+ORIGIN_CITY_IATA = "LON"
+
+current_time = datetime.now()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+from_date = (current_time + timedelta(days=3)).strftime('%d/%m/%Y')
+to_date = (current_time + timedelta(days=180)).strftime('%d/%m/%Y')
+
+for destination in sheet_data:
+    flight = flight_search.search_fare_price(
+        origin_code=ORIGIN_CITY_IATA,
+        destination_city_code=destination["iataCode"],
+        from_date=from_date,
+        to_date=to_date
+    )
 
 
