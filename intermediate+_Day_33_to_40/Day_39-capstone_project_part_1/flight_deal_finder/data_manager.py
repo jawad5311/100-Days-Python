706fbf5248ea25ae957d7098421d6b8217dc6d32
Jawad <muhammadjawad5311@gmail.com>
2021-05-25 11:05:55 +0500

    Flight Dead Finder | sheety put request successfull

--- intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/data_manager.py
+++ intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/data_manager.py
@@ -1,3 +1,80 @@
-class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+import requests
+import os
+
+from flight_search import FlightSearch
+
+import dotenv
+dotenv.load_dotenv()
+
+sheety_endpoint = os.environ.get('SHEETY_ENDPOINT')
+sheety_api = os.environ.get('SHEETY_API')
+
+
+sheety_header = {
+    'Authorization': f'Bearer {sheety_api}'
+}
+
+
+class DataManager(FlightSearch):
+    def __init__(self):
+        self.destination_data = [
+          {'city': 'Paris', 'iataCode': '', 'id': 2, 'lowestPrice': 54},
+          {'city': 'Berlin', 'iataCode': '', 'id': 3, 'lowestPrice': 42},
+          {'city': 'Tokyo', 'iataCode': '', 'id': 4, 'lowestPrice': 485},
+          {'city': 'Sydney', 'iataCode': '', 'id': 5, 'lowestPrice': 551},
+          {'city': 'Istanbul', 'iataCode': '', 'id': 6, 'lowestPrice': 95},
+          {'city': 'Kuala Lumpur', 'iataCode': '', 'id': 7, 'lowestPrice': 414},
+          {'city': 'New York', 'iataCode': '', 'id': 8, 'lowestPrice': 240},
+          {'city': 'San Francisco', 'iataCode': '', 'id': 9, 'lowestPrice': 260},
+          {'city': 'Cape Town', 'iataCode': '', 'id': 10, 'lowestPrice': 378}]
+
+
+    # def get_destination_data(self):
+    #     response = requests.get(sheety_endpoint)
+    #     data = response.json()['sheet1']
+    #     self.destination_data = data
+    #     return self.destination_data
+
+    def update_destination_code(self):
+        print(self.destination_data)
+        for row in self.destination_data:
+            city = row['city']
+            print(city)
+            print(type(city))
+            iataCode = row['iataCode']
+            if iataCode == '':
+                new_data = {
+                    'sheet1': {
+                        'iataCode': FlightSearch.get_destination_code(city)
+                    }
+                }
+                response = requests.put(
+                    url=f"{sheety_endpoint}/{row['id']}",
+                    headers= sheety_header,
+                    json = new_data
+                )
+                print(response.status_code)
+                print(response.text)
+
+        print(self.destination_data)
+
+
+# def destination_code():
+#     # city = "TESTING"
+#     return "testing"
+
+# def update_destination_code():
+#     print(destination_data)
+#     for row in destination_data:
+#         city = row['city']
+#         print(city)
+#         print(type(city))
+#         iataCode = row['iataCode']
+#         if iataCode == '':
+#             row['iataCode'] = destination_code()
+#
+#     print(destination_data)
+
+
+# update_destination_code()
--- intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/main.py
+++ intermediate+_Day_33_to_40/Day_39-capstone_project_part_1/flight_deal_finder/main.py
@@ -1,8 +1,10 @@
 
-
 import os
 import requests
-import pprint
+from pprint import pprint
+
+import flight_search
+import data_manager
 
 import dotenv
 dotenv.load_dotenv()
@@ -13,11 +15,54 @@ kiwi_api = os.environ.get('KIWI_API')
 
 sheety_endpoint = os.environ.get('SHEETY_ENDPOINT')
 
-response = requests.get(sheety_endpoint)
-sheet_data = response.json()['sheet1']
 
-pprint.pprint(sheet_data)
+data_manager = data_manager.DataManager().update_destination_code()
+flight_search = flight_search.FlightSearch()
+
+# data = data_manager.get_destination_data()
+# # pprint(data)
+#
+# data = data_manager.update_destination_code()
 
 
+# response = requests.get(sheety_endpoint)
+# sheet_data = response.json()  #['sheet1']
+
+#
+# sheet_data = [{'city': 'Paris', 'iataCode': '', 'id': 2, 'lowestPrice': 54},
+#               {'city': 'Berlin', 'iataCode': '', 'id': 3, 'lowestPrice': 42},
+#               {'city': 'Tokyo', 'iataCode': '', 'id': 4, 'lowestPrice': 485},
+#               {'city': 'Sydney', 'iataCode': '', 'id': 5, 'lowestPrice': 551},
+#               {'city': 'Istanbul', 'iataCode': '', 'id': 6, 'lowestPrice': 95},
+#               {'city': 'Kuala Lumpur', 'iataCode': '', 'id': 7, 'lowestPrice': 414},
+#               {'city': 'New York', 'iataCode': '', 'id': 8, 'lowestPrice': 240},
+#               {'city': 'San Francisco', 'iataCode': '', 'id': 9, 'lowestPrice': 260},
+#               {'city': 'Cape Town', 'iataCode': '', 'id': 10, 'lowestPrice': 378}]
+
+# for data in sheet_data:
+#     city_code = data['iataCode']
+#     # print(city_code == '')
+#     if city_code == '':
+#         data['iataCode'] = flight_search.destination_code(data['city'])
+#
+# pprint(sheet_data)
+
+# pprint.pprint(sheet_data)
+
+# iata = flight_search.FlightSearch()
+#
+#
+# sheet_data = data_manager.get_destination_data()
 
 
+# print(sheet_data)
+
+# code = iata.IATAcode()
+#
+# print(code)
+
+# for item in sheet_data:
+#     item = item['iataCode']
+#     print(item is None)
+#
+# print(item['city'])
