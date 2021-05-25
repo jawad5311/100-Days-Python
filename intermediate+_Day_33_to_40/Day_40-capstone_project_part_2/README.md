9eef7656338c17d44d8555b33387b0e3860f5020
Jawad <muhammadjawad5311@gmail.com>
2021-05-25 23:35:28 +0500

    Flight Club | sheety post request successful

---
+++ intermediate+_Day_33_to_40/Day_40-capstone_project_part_2/README.md
@@ -0,0 +1,7 @@
+# Flight Club - Capstone Part 2
+
+### Day 40 Goals
+
+1. Make users sign up
+
+2. Send them flight deals using email
---
+++ intermediate+_Day_33_to_40/Day_40-capstone_project_part_2/main.py
@@ -0,0 +1,47 @@
+
+import requests
+import os
+import dotenv
+dotenv.load_dotenv()
+
+sheety_endpoint = os.environ.get('SHEETY_USER_ENDPOINT')
+sheety_api = os.environ.get('SHEETY_USER_API')
+sheety_header = {
+    'Authorization': f"Bearer {sheety_api}"
+}
+
+
+# response = requests.get(
+#     url=sheety_endpoint,
+#     headers=sheety_header,
+# )
+#
+# print(response.json())
+
+user_FN = input(f"Your First Name? ").title()
+user_LN = input(f"Your Last Name? ").title()
+user_email = input(f"Your Email? ").lower()
+user_email_again = input(f"Type your Email again? ").lower()
+
+if user_email == user_email_again:
+    print(f"\nWelcome to the club")
+else:
+    print(f"Please enter the right email...")
+
+sheety_parameters_post = {
+    'user':{
+        'firstName': user_FN,
+        'lastName': user_LN,
+        'email': user_email
+    }
+}
+
+post_response = requests.post(
+    url=sheety_endpoint,
+    headers=sheety_header,
+    json=sheety_parameters_post
+)
+
+print(post_response.status_code)
+print(post_response.text)
+
