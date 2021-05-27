9a6d1a65b94fdb3a132d6e33cdea50b3a561fd3f
Jawad <muhammadjawad5311@gmail.com>
2021-05-27 22:26:14 +0500

    Got URIs for all available songs from the list

--- intermediate+_Day_45_to_57/Day_46-musical_time_machine_project/musical_time_machine/main.py
+++ intermediate+_Day_45_to_57/Day_46-musical_time_machine_project/musical_time_machine/main.py
@@ -11,29 +11,29 @@ sp_client_id = os.getenv('CLIENT_ID')
 sp_client_key = os.getenv('CLIENT_KEY')
 redirect_uri = 'http://localhost'
 
-# billboard = "https://www.billboard.com/charts/hot-100/"
-# date_year = input(f"Where would you like to travel?\nType the date "
-#              f"YYYY-MM-DD: ")
-# date_month = input(f"Month in Numbers: ")
-# date_day = input(f"Day in Numbers: ")
-#
-# url = f"{billboard}{date_year}-{date_month}-{date_day}"
-# print(url)
-#
-# # data = open('data.html')
-#
-# response = requests.get(url)
-# print(response.status_code)
-# # print(response.text)
-#
-#
-# soup = bs4.BeautifulSoup(response.text, 'html.parser')
-# songs = soup.find_all('span', class_='chart-element__information__song')
-# # print(len(songs))
-#
-# songs_list = [song.text for song in songs]
-#
-# print(songs_list)
+billboard = "https://www.billboard.com/charts/hot-100/"
+date_year = input(f"Where would you like to travel?\nType the date "
+             f"YYYY-MM-DD: ")
+date_month = input(f"Month in Numbers: ")
+date_day = input(f"Day in Numbers: ")
+
+url = f"{billboard}{date_year}-{date_month}-{date_day}"
+print(url)
+
+# data = open('data.html')
+
+response = requests.get(url)
+print(response.status_code)
+# print(response.text)
+
+
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+songs = soup.find_all('span', class_='chart-element__information__song')
+# print(len(songs))
+
+songs_list = [song.text for song in songs]
+
+print(songs_list)
 
 songs_list = ['Incomplete', 'Bent', "It's Gonna Be Me", "Jumpin', Jumpin'", 'Try Again', 'I Wanna Know',
               'Everything You Want', 'Absolutely (Story Of A Girl)', 'Higher', "Doesn't Really Matter", 'I Need You',
@@ -65,8 +65,17 @@ sp = spotipy.Spotify(
         cache_path="token.txt"
     )
 )
-user_id = sp.current_user()["id"]
-print(user_id)
 
-track = sp.search('incomplete')
-print(type(track))
+song_uris = []
+
+for song in songs_list:
+    result = sp.search(f"track:{song} year:{date_year}", type='track')
+    # print(result)
+    try:
+        uri = result['tracks']['items'][0]['uri']
+        song_uris.append(uri)
+    except IndexError:
+        print(f"Track \"{song}\" doesn't exist in Spotify. Skipped")
+
+print(song_uris)
+print(len(song_uris))
