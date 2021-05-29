cf14dc889653f7c645bdbd49738ccf6ff0f17307
Jawad <muhammadjawad5311@gmail.com>
2021-05-29 20:45:55 +0500

    Twitter Complaint Bot | Project completed & code formatted

--- intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/README.md
+++ intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/README.md
@@ -1,1 +1,5 @@
- 
+# Complaining Twitter Bot
+
+### Day 51 Goals
+
+1. Project - Complaining Twitter Bot
--- intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/internet_speed_complaint_bot/main.py
+++ intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/internet_speed_complaint_bot/main.py
@@ -1,4 +1,13 @@
 
+"""
+    Twitter Complaint Bot for Internet Speed
+
+    Measure the current speed and compare is with the advertised speed.
+    If current speed less than advertised speed.
+    Tweet a complaint to ISP.
+
+"""
+
 from selenium import webdriver
 import time
 
@@ -9,70 +18,63 @@ dotenv.load_dotenv()
 
 # CONSTANTS Variables
 PROMISED_DOWN = 5
-PROMISED_UP = 70
+PROMISED_UP = 700
 email = os.getenv('EMAIL_TWITTER')
 password = os.getenv('PASSWORD_TWITTER')
 chrome_driver_path = 'C:/Development/chromedriver.exe'
 
 
 class TwitterBot:
+    """ Initialize Selenium setup, get current internet speed and post tweet """
     def __init__(self):
-        self.driver = webdriver.Chrome(chrome_driver_path)
-        self.driver.set_window_size(1310, 720)
+        self.driver = webdriver.Chrome(chrome_driver_path)  # Initialize web driver
+        self.driver.set_window_size(1310, 720)  # Initialize window size
         self.down = 0
         self.up = 0
 
     def get_internet_speed(self):
+        """ Get current internet speed and update down and up speeds """
         self.driver.get('https://fast.com/')
-
-        time.sleep(40)
-
+        time.sleep(40)  # sleeps for 40 sec to let the test complete
         show_more_info_btn = self.driver.find_element_by_id('show-more-details-link')
         show_more_info_btn.click()
+        self.down = int(round(float(self.driver.find_element_by_id('speed-value').text)))  # Get download speed
+        self.up = int(round(float(self.driver.find_element_by_id('upload-value').text)))  # Get Upload speed
+        self.driver.close()  # Closes the windwon after the test is completed
 
-        self.down = int(round(float(self.driver.find_element_by_id('speed-value').text)))
-        self.up = int(round(float(self.driver.find_element_by_id('upload-value').text)))
-
-        self.driver.close()
-
-        # print(down_speed)
-        # print(up_speed)
+        if self.down < PROMISED_DOWN and self.up < PROMISED_UP:
+            self.post_tweet()
 
     def post_tweet(self):
-        # print(self.down)
-        # print(self.up)
-
-        self.driver.get('https://twitter.com/?lang=en')
-        login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[3]/a[2]/div/span/span')
-        time.sleep(3)
-        login_btn.click()
-
-        time.sleep(1)
-
-        email_col = self.driver.find_element_by_name('session[username_or_email]')
-        email_col.send_keys(email)
-
-        password_col = self.driver.find_element_by_name('session[password]')
-        password_col.send_keys(password)
-
+        """ Login to twitter and post tweet """
+        self.driver.get('https://twitter.com/login')
+        time.sleep(2)
+        # Get hold of email entry and type in the email
+        email_entry = self.driver.find_element_by_name('session[username_or_email]')
+        email_entry.send_keys(email)
+        # Get hold of password entry and type in the password
+        password_entry = self.driver.find_element_by_name('session[password]')
+        password_entry.send_keys(password)
+        # Grab login button and click it
         login_btn_2 = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div/span/span')
         login_btn_2.click()
-
         time.sleep(5)
-
+        # Go to the tweet section and type in the msg
         tweet_section = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
-        tweet_section.send_keys('Good Evening everyone! \nTell me something '
-                                'better than an evening chai\n#chai ')
-        # self.driver.close()
-        time.sleep(10)
+        tweet_section.send_keys(
+            f"Why I am getting {self.down} Mbps down and {self.up} Kbps up when I am paying for"
+            f" 8 Mbps down and 1 Mbps up??? @ptcl.\n"
+            f"#Disappointed")
+        time.sleep(1)
+        # Locate and click tweet button
         tweet_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span')
         tweet_btn.click()
         time.sleep(5)
-
+        self.driver.quit()  # Closes the window
 
 
 tb = TwitterBot()
 
-# tb.get_internet_speed()
-tb.post_tweet()
+tb.get_internet_speed()
+# tb.post_tweet()
 
