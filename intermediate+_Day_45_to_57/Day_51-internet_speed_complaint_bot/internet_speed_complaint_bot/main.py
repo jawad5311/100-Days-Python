5a78368f22e8f4e7543fa85427b9f79078228cfc
Jawad <muhammadjawad5311@gmail.com>
2021-05-29 15:45:07 +0500

    Class created and initialized to get internet speed and post tweet

--- intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/internet_speed_complaint_bot/main.py
+++ intermediate+_Day_45_to_57/Day_51-internet_speed_complaint_bot/internet_speed_complaint_bot/main.py
@@ -1,5 +1,6 @@
 
 from selenium import webdriver
+import time
 
 import os
 import dotenv
@@ -11,11 +12,26 @@ PROMISED_DOWN = 5
 PROMISED_UP = 1
 email = os.getenv('EMAIL_TWITTER')
 password = os.getenv('PASSWORD_TWITTER')
-
-
 chrome_driver_path = 'C:/Development/chromedriver.exe'
-driver = webdriver.Chrome(chrome_driver_path)
-driver.set_window_size(1340, 720)
+
+
+class TwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome(chrome_driver_path)
+        self.driver.set_window_size(1340, 720)
+        self.down = 0
+        self.up = 0
+
+    def get_internet_speed(self):
+        # self.driver.get('https://fast.com/')
+        # time.sleep(60)
+        # down_speed = self.driver.find_element_by_id('speed-value')
+        # up_speed = self.driver.find_element_by_id('upload-value')
+        # # print(down_speed)
+
+    def post_tweet(self):
+        pass
+
 
 
 
