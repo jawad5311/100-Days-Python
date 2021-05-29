ab2f038b6c02623a74e5d720a2292d6428d37a83
Jawad <muhammadjawad5311@gmail.com>
2021-05-29 22:59:56 +0500

    IG Follower Bot | code changed to the class & methods

--- intermediate+_Day_45_to_57/Day_52-instagram_follower_bot/ig_follower_bot/main.py
+++ intermediate+_Day_45_to_57/Day_52-instagram_follower_bot/ig_follower_bot/main.py
@@ -11,20 +11,38 @@ dotenv.load_dotenv()
 email = os.getenv('EMAIL_IG')
 password = os.getenv('PASSWORD_IG')
 
-
-driver = webdriver.Chrome('C:/Development/chromedriver.exe')
-driver.set_window_size(1310, 720)
-
-driver.get('https://www.instagram.com/')
-time.sleep(5)
-email_entry = driver.find_element_by_name('username')
-email_entry.send_keys(email)
-email_entry.send_keys(Keys.ENTER)
-
-password_entry = driver.find_element_by_name('password')
-password_entry.send_keys(password)
-password_entry.send_keys(Keys.ENTER)
+similar_account = ''
 
 
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Chrome('C:/Development/chromedriver.exe')
+        self.driver.set_window_size(1310, 720)
 
+    def login(self):
+        self.driver.get('https://www.instagram.com/')
+        time.sleep(3)
+        email_entry = self.driver.find_element_by_name('username')
+        email_entry.send_keys(email)
+
+        password_entry = self.driver.find_element_by_name('password')
+        password_entry.send_keys(password)
+        password_entry.send_keys(Keys.ENTER)
+        time.sleep(5)
+        close_popup = self.driver.find_element_by_css_selector('.mt3GC .HoLwm')
+        time.sleep(1)
+        close_popup.click()
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+
+
+
+# search_bar = driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+# search_bar.send_keys('python enthusiast')
+# search_bar.send_keys(Keys.ENTER)
 
