68239dc5b0c1a7b079dfc198e70eb642bea45a29
Jawad <muhammadjawad5311@gmail.com>
2021-05-30 22:34:03 +0500

    Data Entry job automation | Project completed & code formatted

--- intermediate+_Day_45_to_57/Day_53-job_automation/data_entry_automation_project/main.py
+++ intermediate+_Day_45_to_57/Day_53-job_automation/data_entry_automation_project/main.py
@@ -1,65 +1,73 @@
 
+import time
+
 import requests
 import bs4
 from selenium import webdriver
 
-import os
-import dotenv
-
-dotenv.load_dotenv()
-
-
+# Request data from zillow.com
 response_zillow = requests.get(
     url='https://www.trulia.com/for_rent/San_Francisco,CA/0-1200_price/'
 )
 
-# print(response_zillow.text)
-
-print(response_zillow.status_code)
+print(response_zillow.status_code)  # Return the status of the request
 
 soup = bs4.BeautifulSoup(response_zillow.text, 'html.parser')
-# print(soup.prettify())
 
+# Holds all of the required data from the site
 rent_prices = soup.find_all(attrs={"data-testid": "property-price"})
 property_address_1 = soup.find_all(attrs={"data-testid": "property-street"})
 property_address_2 = soup.find_all(attrs={"data-testid": "property-region"})
 links = soup.find_all(name='a', attrs={"data-testid": "property-card-link"})
 
+# Creates the lists of the scrapped data
 property_rents = [rent.text for rent in rent_prices]
 property_addr_1 = [adr.text for adr in property_address_1]
 property_addr_2 = [adr.text for adr in property_address_2]
 property_href = [link['href'] for link in links]
 
-# print(len(property_rents))
-print(property_rents)
-# print(len(property_addr_1))
-# print(property_addr_1)
-# print(len(property_addr_2))
-# print(property_addr_2)
-# print(len(property_href))
-# print(property_href)
-
-# property_rents = ['$900 - $1,045/mo', '$895/mo', '$1,145 - $1,995/mo', 'Contact For Price', '$1,195/mo', '$1,150/mo', '$1,195/mo', '$1,195/mo', '$1,100/mo', '$1,200/mo', '$1,200/mo', '$1,195 - $1,395/mo', '$1,195/mo', '$1,200/mo', '$1,095 - $1,730/mo', '$695/mo', '$1,200/mo', '$1,100/mo']
-# property_addr_1 = ['1000-1010 Bush Street Apartments', 'Studio, $895', '1029 Geary St. Apartments', 'Bayside Village Apartments', '400 Broadway', '979 Sutter Street', 'Beautiful Furnished Rooms in North Beach', 'Beautiful Community Living in SoMa', '118 Trumbull St', '106 Fern St #12', '2475 19th Ave', 'Studio, $1,395', '624 Ellis St #9', '80 Raymond Ave', 'Common City Gardens Apartments', '1114 Sutter St #10', 'Address Not Disclosed', '109 San Diego Ave']
-# property_addr_2 = ['Lower Nob Hill, San Francisco, CA', 'Chinatown, San Francisco, CA', 'Tenderloin, San Francisco, CA', 'South Beach, San Francisco, CA', 'North Beach, San Francisco, CA', 'Lower Nob Hill, San Francisco, CA', 'North Beach, San Francisco, CA', 'South of Market, San Francisco, CA', 'Mission Terrace, San Francisco, CA', 'Lower Nob Hill, San Francisco, CA', 'Parkside, San Francisco, CA', 'Tenderloin, San Francisco, CA', 'Tenderloin, San Francisco, CA', 'Visitacion Valley, San Francisco, CA', 'South of Market, San Francisco, CA', 'Lower Nob Hill, San Francisco, CA', 'Daly City, CA', 'Daly City, CA']
-# property_links = ['/c/ca/san-francisco/1000-1010-bush-street-1000-1010-bush-st-san-francisco-ca-94109--2379258280', '/c/ca/san-francisco/studio-895-706-kearny-st-san-francisco-ca-94111--2468098141', '/c/ca/san-francisco/1029-geary-st-1029-geary-st-san-francisco-ca-94109--1004214860', '/c/ca/san-francisco/bayside-village-3-bayside-village-pl-san-francisco-ca-94107--2082867438', '/p/ca/san-francisco/400-broadway-san-francisco-ca-94133--2083093886', '/c/ca/san-francisco/979-sutter-street-979-sutter-st-san-francisco-ca-94109--2082892821', '/c/ca/san-francisco/beautiful-furnished-rooms-in-north-beach-371-columbus-ave-san-francisco-ca-94133--2083093624', '/c/ca/san-francisco/beautiful-community-living-in-soma-251-9th-st-san-francisco-ca-94103--2179492561', '/p/ca/san-francisco/118-trumbull-st-san-francisco-ca-94112--1139029200', '/p/ca/san-francisco/106-fern-st-12-san-francisco-ca-94109--2320594935', '/p/ca/san-francisco/2475-19th-ave-san-francisco-ca-94116--2082974408', '/c/ca/san-francisco/studio-1-395-344-ellis-st-san-francisco-ca-94102--2535985289', '/p/ca/san-francisco/624-ellis-st-9-san-francisco-ca-94109--2534978084', '/p/ca/san-francisco/80-raymond-ave-san-francisco-ca-94134--2083109105', '/c/ca/san-francisco/common-city-gardens-333-12th-st-san-francisco-ca-94103--2082848365', '/p/ca/san-francisco/1114-sutter-st-10-san-francisco-ca-94109--2173378859', '/p/ca/daly-city/address-not-disclosed-daly-city-ca-94014--2082638946', '/p/ca/daly-city/109-san-diego-ave-daly-city-ca-94014--2082630745']
-
+# Create a whole single address from the 2 addresses scrapped from the site
 property_addresses = []
-
 for i in range(len(property_addr_2)):
     addr_1 = property_addr_1[i]
     addr_2 = property_addr_2[i]
 
-    addr = f"{addr_1}, {addr_2}"
+    addr = f"{addr_1}, {addr_2}"  # Combines the street and city address
     property_addresses.append(addr)
 
-print(property_addresses)
-
+# Creates a complete url from of the scrapped incomplete href
 property_links = []
-
 for i in range(len(property_href)):
     web_url = 'https://www.trulia.com/'
-    complete_url = f"{web_url}{property_href[i]}"
+    complete_url = f"{web_url}{property_href[i]}"  # Adds trulia.com in front of each scrapped link
     property_links.append(complete_url)
 
-print(property_links)
+# Google form link
+form_link = 'https://docs.google.com/forms/d/e/1FAIpQLSftG1mLvbYxRS7citmAKV85gv_Ve2N7ia6ihoTXuMeDoQtFVA/viewform?usp=sf_link'
+
+# Creates and Initialize chrome web driver
+driver = webdriver.Chrome('c:/Development/chromedriver.exe')
+driver.set_window_size(1300, 720)
+
+# Submits all of the scrapped data to the google form using selenium
+for i in range(len(property_addresses)):
+    driver.get(form_link)  # Go to the google form link
+    time.sleep(1)
+
+    # Get holds of entries and submit buttons
+    address_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    rent_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_entry = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_btn = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span/span')
+
+    # Sends the relevant data to its entry column
+    address_entry.send_keys('')  # Send an empty string to focus in the entry
+    address_entry.send_keys(property_addresses[i])
+    rent_entry.send_keys('')  # Send an empty string to focus in the entry
+    rent_entry.send_keys(property_rents[i])
+    link_entry.send_keys('')  # Send an empty string to focus in the entry
+    link_entry.send_keys(property_links[i])
+
+    submit_btn.click()  # Clicks the submit button
+    print("From submitted!")
+
