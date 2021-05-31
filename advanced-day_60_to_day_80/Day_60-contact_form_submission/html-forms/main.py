2668577495d53993f84484527389cbee7387ccc9
Jawad <muhammadjawad5311@gmail.com>
2021-05-31 23:00:34 +0500

    HTML form submission using flask | project completed

--- advanced-day_60_to_day_80/Day_60-contact_form_submission/html-forms/main.py
+++ advanced-day_60_to_day_80/Day_60-contact_form_submission/html-forms/main.py
@@ -1,39 +1,50 @@
 
+import smtplib
+import os
+import dotenv
+
 import flask
 
-app = flask.Flask(__name__)
+
+dotenv.load_dotenv()  # Loads Environment Variables
+
+# Get email and password from Environment Variables
+email_from = os.environ.get('USER_EMAIL')
+password = os.environ.get('USER_PASS')
+email_to = os.environ.get('TO_EMAIL')
+
+app = flask.Flask(__name__)  # Creates flask app
 
 
-@app.route('/')
+@app.route('/')  # Route to the home page
 def home():
     return flask.render_template('index.html')
 
 
-# @app.route('/contact')
-# def contact_page():
-#     return flask.render_template('contact.html')
-
-
-@app.route('/contact', methods=['POST', 'GET'])
+@app.route('/contact', methods=['POST', 'GET'])  # Contact Page
 def form_submitted():
+    """ Form submission check and send mail """
     if flask.request.method == 'POST':
         data = flask.request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["number"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["number"], data["message"])
         return "<h1>Successfully sent your message</h1>"
     return flask.render_template("contact.html")
 
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])  # User Login Page
 def receive_form_data():
     username = flask.request.form['username']
     password = flask.request.form['password']
     return f"{username}  {password}"
 
 
+def send_email(name, email, phone, message):
+    """ Send email to myself """
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.live.com") as connection:
+        connection.starttls()
+        connection.login(email_from, password)
+        connection.sendmail(email_from, email_to, email_message)
 
 
 if __name__ == '__main__':
