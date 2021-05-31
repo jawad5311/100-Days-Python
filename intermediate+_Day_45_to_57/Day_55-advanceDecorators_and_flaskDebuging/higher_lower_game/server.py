157db21bf9439ab0d9e07fe1ab725fc683d6b403
Jawad <muhammadjawad5311@gmail.com>
2021-05-31 12:57:04 +0500

    Higher Lower Game completed

--- intermediate+_Day_45_to_57/Day_55-advanceDecorators_and_flaskDebuging/higher_lower_game/server.py
+++ intermediate+_Day_45_to_57/Day_55-advanceDecorators_and_flaskDebuging/higher_lower_game/server.py
@@ -5,23 +5,30 @@ import random
 app = flask.Flask(__name__)
 
 
-def make_heading_1(func):
-    def wrapper():
-        return f"<h1>{func()}</h1>" \
-               f"<img src='ahttps://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
-
-    return wrapper
-
-
-@app.route('/')
-@make_heading_1
+@app.route('/')  # Route to the Home Page
 def game_start():
-    return "Guess a number between 0 and 9"
+    return "Guess a number between 0 and 9<br>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
 
 
+# Creates a random number between 0 and 9
+random_num = random.randint(0, 9)
+print(random_num)
 
 
-
+@app.route('/<int:guess>')  # Creates a URL on user input
+def user_guess(guess):
+    if guess > random_num:
+        return f"<h1>{guess} is not the correct answer!<br>" \
+               f"<h1>Too High. Go down ⬇️</h1><br>" \
+               f"<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
+    elif guess < random_num:
+        return f"<h1>{guess} is not the correct answer!<br>" \
+               f"Too Low. Go up ⬆️</h1><br>" \
+               f"<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
+    else:
+        return f"<h1>You guessed the number <em>{random_num}</em> right!</h1><br><br>" \
+               f"<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
 
 
 if __name__ == '__main__':
