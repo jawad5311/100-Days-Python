1529e8a51ee63041377dc26df3abad37ff3a4979
Jawad <muhammadjawad5311@gmail.com>
2021-05-31 22:05:08 +0500

    contact form data recieved using post method

--- advanced-day_60_to_day_80/Day_60-contact_form_submission/html-forms/main.py
+++ advanced-day_60_to_day_80/Day_60-contact_form_submission/html-forms/main.py
@@ -9,7 +9,29 @@ def home():
     return flask.render_template('index.html')
 
 
+# @app.route('/contact')
+# def contact_page():
+#     return flask.render_template('contact.html')
 
+
+@app.route('/contact', methods=['POST', 'GET'])
+def form_submitted():
+    if flask.request.method == 'POST':
+        data = flask.request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["number"])
+        print(data["message"])
+        return "<h1>Successfully sent your message</h1>"
+    return flask.render_template("contact.html")
+
+
+
+@app.route('/login', methods=['POST'])
+def receive_form_data():
+    username = flask.request.form['username']
+    password = flask.request.form['password']
+    return f"{username}  {password}"
 
 
 
