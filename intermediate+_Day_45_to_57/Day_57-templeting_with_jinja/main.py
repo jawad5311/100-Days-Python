c261d548081054133ee8c6975c563df400bd9eea
Jawad <muhammadjawad5311@gmail.com>
2021-05-31 15:33:19 +0500

    Challenge Jinja & API | user age and gender get successfully

--- intermediate+_Day_45_to_57/Day_57-templeting_with_jinja/main.py
+++ intermediate+_Day_45_to_57/Day_57-templeting_with_jinja/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import datetime
+import requests
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,29 @@ app = Flask(__name__)
 def home():
     time = datetime.date.today().year
     return render_template('index.html', year=time)
+
+
+@app.route('/guess/<name>')
+def get_name_data(name):
+
+    def get_gender(user_name):
+        url = f"https://api.genderize.io?name={user_name}"
+        response = requests.get(url)
+        gender = response.json()['gender']
+        return gender
+
+    def get_age(user_name):
+        url = f"https://api.agify.io?name={user_name}"
+        response = requests.get(url)
+        age = str(response.json()['age'])
+        return age
+
+    # user_gender = get_gender(name)
+    user_age = get_age(name)
+    return user_age
+
+
+
 
 
 if __name__ == "__main__":
